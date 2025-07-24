61f0d11f21650de81ec09c0324c587e7dc3dc143
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-07-24 15:04:52 +0530

    updated codepipeline files

---
+++ main.tf
@@ -0,0 +1,113 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "artifact_bucket" {
+  bucket = "${var.project_name}-artifacts"
+  force_destroy = true
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.project_name}-codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+resource "aws_codebuild_project" "this" {
+  name          = "${var.project_name}-build"
+  service_role  = aws_iam_role.codebuild_role.arn
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+  }
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "${var.project_name}-pipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+}
+
+resource "aws_codepipeline" "this" {
+  name     = "${var.project_name}-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  artifact_store {
+    location = aws_s3_bucket.artifact_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "GitHub_Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = split("/", var.github_repo)[0]
+        Repo       = split("/", var.github_repo)[1]
+        Branch     = var.github_branch
+        OAuthToken = var.github_token
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "CodeBuild"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.this.name
+      }
+    }
+  }
+}
---
+++ output.tf
@@ -0,0 +1,24 @@
+output "artifact_bucket_name" {
+  description = "S3 bucket used for storing CodePipeline artifacts"
+  value       = aws_s3_bucket.artifact_bucket.bucket
+}
+
+output "codebuild_project_name" {
+  description = "Name of the AWS CodeBuild project"
+  value       = aws_codebuild_project.this.name
+}
+
+output "codepipeline_name" {
+  description = "Name of the AWS CodePipeline"
+  value       = aws_codepipeline.this.name
+}
+
+output "codepipeline_role_arn" {
+  description = "IAM role ARN for CodePipeline"
+  value       = aws_iam_role.codepipeline_role.arn
+}
+
+output "codebuild_role_arn" {
+  description = "IAM role ARN for CodeBuild"
+  value       = aws_iam_role.codebuild_role.arn
+}
