820519550a0b5d010d1f6bb16f5ede67f8c1f2a3
vijaylaxmi.radder <vijaylaxmi.radder@volunteer.heartfulness.org>
2025-08-22 21:18:50 +0530

    updated files for iam roles

--- main.tf
+++ main.tf
@@ -37,31 +37,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
 
-resource "aws_codebuild_project" "this" {
-  name         = "${var.project_name}-${var.environment}-build"
-  service_role = aws_iam_role.codebuild_role.arn
 
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-  environment {
-    compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:5.0"
-    type            = "LINUX_CONTAINER"
-    privileged_mode = true
-  }
-
-  source {
-    type      = "CODEPIPELINE"
-    buildspec = "buildspec.yml"
-  }
-
-  tags = {
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
 
 resource "aws_iam_role" "codepipeline_role" {
   name = "${var.project_name}-${var.environment}-pipeline-role"
@@ -178,6 +154,7 @@ resource "aws_codebuild_project" "terraform_build" {
 
   tags = {
     Environment = var.environment
+    Project     = var.project_name
   }
 }
 
