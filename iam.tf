003f5128f45a0577a403d24fe9fd5b92bc8b3ad9
vijaylaxmi.radder <vijaylaxmi.radder@volunteer.heartfulness.org>
2025-08-23 00:31:19 +0530

    modified file

--- iam.tf
+++ iam.tf
@@ -163,3 +163,30 @@ resource "aws_iam_role_policy" "codebuild_secrets_access" {
     ]
   })
 }
+
+resource "aws_iam_role_policy" "codebuild_logs_policy" {
+  name = "codebuild-cloudwatch-logs"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = [
+          "arn:aws:logs:ap-south-1:502390415551:log-group:/aws/codebuild/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_logs_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeBuildDeveloperAccess"
+}
