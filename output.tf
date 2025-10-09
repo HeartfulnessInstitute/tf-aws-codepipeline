86f0cf4d71dff5ebf17c0a7d2e433bae7a9c6a41
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-09 16:01:19 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,24 +2,24 @@ output "artifact_bucket_name" {
   description = "S3 bucket used for storing CodePipeline artifacts"
   value       = aws_s3_bucket.artifact_bucket.bucket
 }
+output "codebuild_project_name" {
+  description = "Name of the CodeBuild project created by pipeline module"
+  value       = aws_codebuild_project.this.name
+}
 
-
-# output "codebuild_project_name" {
-#   description = "Name of the AWS CodeBuild project"
-#   value       = aws_codebuild_project.this.name
-# }
-
-#  output "codepipeline_name" {
-#    value       = aws_codepipeline.this.name
-#  }
-
+output "codepipeline_name" {
+  description = "Name of the CodePipeline created by pipeline module"
+  value       = aws_codepipeline.this.name
+}
 
 output "codepipeline_role_arn" {
-  description = "IAM role ARN for CodePipeline"
+  description = "ARN of the IAM role used by CodePipeline"
   value       = aws_iam_role.codepipeline_role.arn
 }
 
 output "codebuild_role_arn" {
-  description = "IAM role ARN for CodeBuild"
+  description = "ARN of the IAM role used by CodeBuild"
   value       = aws_iam_role.codebuild_role.arn
 }
+
+
