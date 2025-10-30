5ad432f6dd2b29d8ab64aa2a4dca117c609d53e9
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-30 13:28:40 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,30 +1,83 @@
-
-output "pipeline_id" {
-  description = "ID of the CodePipeline"
-  value       = aws_codepipeline.pipeline.id
+# S3 Outputs
+output "artifact_bucket_id" {
+  description = "ID of the S3 artifact bucket"
+  value       = aws_s3_bucket.artifact_bucket.id
 }
 
-output "pipeline_arn" {
-  description = "ARN of the CodePipeline"
-  value       = aws_codepipeline.pipeline.arn
+output "artifact_bucket_arn" {
+  description = "ARN of the S3 artifact bucket"
+  value       = aws_s3_bucket.artifact_bucket.arn
 }
 
-output "pipeline_name" {
-  description = "Name of the CodePipeline"
-  value       = aws_codepipeline.pipeline.name
+# CodeBuild Outputs
+output "codebuild_project_id" {
+  description = "ID of the CodeBuild project"
+  value       = aws_codebuild_project.this.id
+}
+
+output "codebuild_project_arn" {
+  description = "ARN of the CodeBuild project"
+  value       = aws_codebuild_project.this.arn
 }
 
 output "codebuild_project_name" {
   description = "Name of the CodeBuild project"
-  value       = aws_codebuild_project.build.name
+  value       = aws_codebuild_project.this.name
 }
 
-output "artifact_bucket_name" {
-  description = "Name of the artifact S3 bucket"
-  value       = aws_s3_bucket.artifacts.id
+output "codebuild_role_arn" {
+  description = "ARN of the CodeBuild IAM role"
+  value       = aws_iam_role.codebuild_role.arn
 }
 
-output "artifact_bucket_arn" {
-  description = "ARN of the artifact S3 bucket"
-  value       = aws_s3_bucket.artifacts.arn
+output "codebuild_role_name" {
+  description = "Name of the CodeBuild IAM role"
+  value       = aws_iam_role.codebuild_role.name
+}
+
+# CodePipeline Outputs
+output "codepipeline_id" {
+  description = "ID of the CodePipeline"
+  value       = aws_codepipeline.this.id
+}
+
+output "codepipeline_arn" {
+  description = "ARN of the CodePipeline"
+  value       = aws_codepipeline.this.arn
+}
+
+output "codepipeline_name" {
+  description = "Name of the CodePipeline"
+  value       = aws_codepipeline.this.name
+}
+
+output "codepipeline_role_arn" {
+  description = "ARN of the CodePipeline IAM role"
+  value       = aws_iam_role.codepipeline_role.arn
+}
+
+output "codepipeline_role_name" {
+  description = "Name of the CodePipeline IAM role"
+  value       = aws_iam_role.codepipeline_role.name
+}
+
+# CodeDeploy Outputs
+output "codedeploy_app_id" {
+  description = "ID of the CodeDeploy application"
+  value       = var.enable_codedeploy ? aws_codedeploy_app.this[0].id : null
+}
+
+output "codedeploy_app_name" {
+  description = "Name of the CodeDeploy application"
+  value       = var.enable_codedeploy ? aws_codedeploy_app.this[0].name : null
+}
+
+output "codedeploy_deployment_group_id" {
+  description = "ID of the CodeDeploy deployment group"
+  value       = var.enable_codedeploy ? aws_codedeploy_deployment_group.this[0].id : null
+}
+
+output "codedeploy_role_arn" {
+  description = "ARN of the CodeDeploy IAM role"
+  value       = var.enable_codedeploy ? aws_iam_role.codedeploy_role[0].arn : null
 }
