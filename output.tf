64a3a927c6db7b6d8d5319b0c8edf3fd9aa9f648
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-30 15:45:10 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,13 +5,8 @@ output "build_project_name" {
 output "terraform_build_project_name" {
   value = aws_codebuild_project.terraform_build.name
 }
-output "app_name" {
-  value = aws_codedeploy_app.donation_app.name
-}
 
-output "deployment_group_name" {
-  value = aws_codedeploy_deployment_group.donation_app_group.deployment_group_name
-}
+
 output "pipeline_name" {
   value = aws_codepipeline.deployment_pipeline.name
 }
