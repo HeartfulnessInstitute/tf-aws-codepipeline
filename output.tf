efe1096ed15e3889e6606f504705da1401bb6406
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-11-11 22:14:27 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,26 @@
-output "build_project_name" {
-  value = aws_codebuild_project.main_build.name
+# --------------------------
+# Outputs
+# --------------------------
+output "policy_arn" {
+  value       = aws_iam_policy.this.arn
+  description = "ARN of the created IAM policy"
 }
 
-output "terraform_build_project_name" {
-  value = aws_codebuild_project.terraform_build.name
+output "attached_role_names" {
+  value       = keys(local.all_role_names_map)
+  description = "Role names the policy was attached to (from role_names and role_arns)"
 }
 
+output "codebuild_main_project_name" {
+  value       = aws_codebuild_project.main_build.name
+  description = "CodeBuild main project name"
+}
 
-output "pipeline_name" {
+output "codebuild_terraform_project_name" {
+  value       = aws_codebuild_project.terraform_build.name
+  description = "CodeBuild terraform project name"
+}
+
+output "codepipeline_name" {
   value = aws_codepipeline.deployment_pipeline.name
-}
-
-output "pipeline_arn" {
-  value = aws_codepipeline.deployment_pipeline.arn
 }
