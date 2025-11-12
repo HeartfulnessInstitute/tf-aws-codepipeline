b8e6414bb4582faf4e7ce632f2c8c1452fd7fa62
anitabai-m <anita.b@volunteer.heartfulness.org>
2025-11-12 13:07:14 +0530

    Refactor outputs in output.tf for CodeBuild and CodePipeline

--- output.tf
+++ output.tf
@@ -1,26 +1,16 @@
-# --------------------------
-# Outputs
-# --------------------------
-output "policy_arn" {
-  value       = aws_iam_policy.this.arn
-  description = "ARN of the created IAM policy"
+output "build_project_name" {
+  value = aws_codebuild_project.main_build.name
 }
 
-output "attached_role_names" {
-  value       = keys(local.all_role_names_map)
-  description = "Role names the policy was attached to (from role_names and role_arns)"
+output "terraform_build_project_name" {
+  value = aws_codebuild_project.terraform_build.name
 }
 
-output "codebuild_main_project_name" {
-  value       = aws_codebuild_project.main_build.name
-  description = "CodeBuild main project name"
-}
 
-output "codebuild_terraform_project_name" {
-  value       = aws_codebuild_project.terraform_build.name
-  description = "CodeBuild terraform project name"
-}
-
-output "codepipeline_name" {
+output "pipeline_name" {
   value = aws_codepipeline.deployment_pipeline.name
+}
+
+output "pipeline_arn" {
+  value = aws_codepipeline.deployment_pipeline.arn
 }
