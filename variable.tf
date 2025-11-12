8e98e1c0c0c7f881c369199a388ea6ceceda39ca
anitabai-m <anita.b@volunteer.heartfulness.org>
2025-11-12 13:07:57 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,178 +1,89 @@
-variable "region" {
-  type        = string
-  description = "AWS region"
-}
-
 variable "environment" {
+  description = "Environment name (dev, staging, prod)"
   type        = string
-  description = "Environment name (dev/prod/stage)."
 }
-
 variable "project_name" {
+  description = "Project name for resource naming"
   type        = string
-  description = "Project name used in naming."
 }
-
-variable "s3_bucket" {
-  type        = string
-  description = "S3 bucket used for artifact access (only bucket name). Leave blank to allow all."
-}
-
-variable "codebuild_project_arn" {
-  type        = string
-  description = "ARN of CodeBuild project to scope codebuild permissions. If empty, the CodeBuild statement will use '*'"
-}
-
-variable "codedeploy_application_arns" {
-  type        = list(string)
-  description = "List of CodeDeploy application ARNs to include in the policy."
-  default     = []
-}
-
-variable "codedeploy_deploymentgroup_arns" {
-  type        = list(string)
-  description = "List of CodeDeploy deployment group ARNs to include in the policy."
-  default     = []
-}
-
-variable "role_names" {
-  type        = list(string)
-  description = "List of IAM role NAMES to attach the policy to (e.g. CodeBuild/CodePipeline role names)."
-  default     = []
-}
-
-variable "role_arns" {
-  type        = list(string)
-  description = "List of IAM role ARNs to attach the policy to (alternative), module extracts the role name from ARN."
-  default     = []
-}
-
-variable "pass_role_resource" {
-  type        = string
-  description = "Resource(s) allowed for iam:PassRole. Default '*' (be stricter if possible)."
-  default     = ""
-}
-
-variable "tags" {
-  description = "Tags to add to resources"
-  type        = map(string)
-  default     = {}
-}
-
-# CodeBuild related variables
 variable "codebuild_role_arn" {
-  type        = string
-  description = "Service role ARN for CodeBuild projects"
+    type = string
 }
 
 variable "image" {
-  type        = string
-  description = "Docker image to use for the app CodeBuild project"
-  default     = "aws/codebuild/standard:7.0"
+  type    = string
+  default = "aws/codebuild/standard:5.0"
 }
 
 variable "terraform_image" {
-  type        = string
-  description = "Docker image to use for terraform CodeBuild project"
-  default     = "hashicorp/terraform:light"
-}
-
-variable "privileged_mode" {
-  type        = bool
-  description = "Whether to enable privileged mode for CodeBuild (for docker builds)"
+  type    = string
+  default = "aws/codebuild/standard:6.0"
 }
 
 variable "buildspec" {
-  type        = string
-  description = "Inline buildspec for the app build or path to buildspec in repo. Default is empty (use project defaults)."
+  type    = string
 }
 
 variable "terraform_buildspec" {
-  type        = string
-  description = "Inline buildspec for the terraform build."
+  type    = string
 }
 
 variable "build_timeout" {
-  type        = number
-  description = "Timeout (minutes) for terraform build project"
-  default     = 60
+  type    = number
 }
 
-variable "build_timeout_compute_type" {
-  type        = string
-  description = "Compute type for terraform build"
-  default     = "BUILD_GENERAL1_SMALL"
+variable "privileged_mode" {
+  type    = bool
+  default = true
 }
-
-variable "codebuild_compute_type" {
-  type        = string
-  description = "Compute type for app build"
-  default     = "BUILD_GENERAL1_SMALL"
-}
-
-# CodeDeploy variables
 variable "codedeploy_role_arn" {
-  type        = string
-  description = "IAM role ARN for CodeDeploy to assume"
+type = string
 }
 
 variable "deployment_config_name" {
-  type        = string
-  description = "Deployment configuration name for CodeDeploy (e.g., CodeDeployDefault.AllAtOnce)"
+  type    = string
+  default = "CodeDeployDefault.AllAtOnce"
 }
 
 variable "ec2_tag_key" {
-  type        = string
-  description = "Tag key to select EC2 instances for deployment"
+type = string
+default = "project"
 }
-
 variable "ec2_tag_value" {
-  type        = string
-  description = "Tag value to select EC2 instances for deployment"
+type = string
 }
-
-# CodePipeline variables
-variable "codepipeline_role_arn" {
-  type        = string
-  description = "IAM role ARN for CodePipeline to assume"
-}
-
 variable "artifact_bucket_name" {
-  type        = string
-  description = "S3 bucket name used as artifact store for CodePipeline"
+type = string
+}
+variable "codepipeline_role_arn" {
+type = string
 }
 
 variable "github_connection_arn" {
-  type        = string
-  description = "CodeStar Connections ARN for GitHub repository"
+type = string
 }
-
 variable "github_owner" {
-  type        = string
-  description = "GitHub repository owner (org or username)"
+type = string
 }
-
-variable "github_repo" {
-  type        = string
-  description = "GitHub repository name"
+variable "github_repo"  {
+type = string
 }
-
 variable "github_branch" {
-  type        = string
-  description = "GitHub branch to use"
+type = string
 }
 
+# consuming CodeBuild/CodeDeploy created elsewhere
 variable "codebuild_project_name" {
-  type        = string
-  description = "Name of the CodeBuild project to run in the pipeline 'Build' stage'"
+type = string
 }
-
-variable "codedeploy_app_name" {
-  type        = string
-  description = "Name of CodeDeploy application (for pipeline action)"
+variable "codedeploy_app_name"   {
+type = string
 }
-
 variable "codedeploy_group_name" {
-  type        = string
-  description = "Name of CodeDeploy deployment group (for pipeline action)"
+type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
